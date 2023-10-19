4aa4e5770a96fea94ef94d34c42507e8c902f9a7
Nora <norapasquale@gmail.com>
2023-10-19 18:49:13 +0200

    mm

--- server.js
+++ server.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
+const port = 3000;
 
-const port = 3000; // You can choose any available port
-
-app.use(express.json());
-
-app.get('/search', (req, res) => {
-  const authorQuery = req.query.author;
-  const titleQuery = req.query.title;
-
-  // Read the JSON data from the file
-  const jsonData = JSON.parse(fs.readFileSync('output.json', 'utf8'));
-
-  const searchResults = jsonData.filter(item => {
-    return (
-      item.Authors.toLowerCase().includes(authorQuery.toLowerCase()) &&
-      item.Title.toLowerCase().includes(titleQuery.toLowerCase())
-    );
-  });
-
-  res.json(searchResults);
-});
+app.use(express.static(__dirname)); // Serve static files from the current directory
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
