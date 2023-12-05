e0ab03c269f218cbb69610cfb830752ad17606bf
Nora <norapasquale@gmail.com>
2023-12-05 18:59:50 +0100

    jj

---
+++ Prova2/Library2.js
@@ -0,0 +1,248 @@
+const jsonData =[{
+    "Id":0,
+    "Authors":"Abba, Giuseppe Cesare",
+    "Title":"Ricordi garibaldini",
+    "Cities":"Torino",
+    "Publisher":"Sten",
+    "Years":1913,
+    "Fund":"BTB Segn. 1493",
+    "Segni":null,
+    "Category":"IL",
+    "Schede":"AppLetIt, AppBibIt"
+},{
+    "Id":1,
+    "Authors":"Abernethy, Silvia M.",
+    "Title":"Something to read. Qualcosa da leggere in inglese. Letture per gli studenti di inglese",
+    "Cities":"Firenze  ",
+    "Publisher":"La Nuova Italia",
+    "Years":1946,
+    "Fund":"BTB Segn. 2404",
+    "Segni":null,
+    "Category":"GR",
+    "Schede":null
+}, {
+    "Id":3,
+    "Authors":"Abraham, Pierre",
+    "Title":"Cr\u00e9atures chez Balzac. Recherches sur la r\u00e9alisation intellectuelle. Avec un texte in\u00e9dit de Balzac",
+    "Cities":"Paris",
+    "Publisher":"NRF-Gallimard",
+    "Years":1931,
+    "Fund":"BTB Segn. 1116.",
+    "Segni":"Nota di possesso autografa in data 10.1931. Pieghe, sottolineature, postille.",
+    "Category":"FL",
+    "schede tematiche":"AppLetFr"
+}];
+
+ const jsonDataArch= [
+    {
+        "N":0,
+        "archivio":"Archivio Bonsanti",
+        "fondo":"Fondo Gadda",
+        "unit\u00e0":"\u00abPiccola antologia 1904\u00bb",
+        "serie":"II",
+        "sottoserie":"1 Quaderni 1",
+        "faldone":null,
+        "opera":"Appunti giovanili, scolastici e universitari (politecnico)",
+        "Schede":"AppLetIt",
+        "lib":null,
+        "luogo":null,
+        "genere":"Appunti",
+        "forma":"Quaderni",
+        "tipo":"Documento",
+        "contenitore":"1 scatola",
+        "sigla":null,
+        "Unnamed: 16":null,
+        "descrizione":null,
+        "provenienza":null,
+        "lingua":null,
+        "sommario":null,
+        "stato di conservazione":null,
+        "collana":null,
+        "data":-2082844800000
+    },  {
+        "N":1,
+        "archivio":"Archivio Bonsanti",
+        "fondo":"Fondo Gadda",
+        "unit\u00e0":"\u00abPiccola antologia 1904\u00bb",
+        "serie":"II",
+        "sottoserie":"1 Quaderni 1",
+        "faldone":null,
+        "opera":"Appunti giovanili, scolastici e universitari (politecnico)",
+        "Schede":"AppBibIt",
+        "lib":null,
+        "luogo":null,
+        "genere":"Appunti",
+        "forma":"Quaderni",
+        "tipo":"Documento",
+        "contenitore":"1 scatola",
+        "sigla":null,
+        "Unnamed: 16":null,
+        "descrizione":null,
+        "provenienza":null,
+        "lingua":null,
+        "sommario":null,
+        "stato di conservazione":null,
+        "collana":null,
+        "data":-2082844800000
+    },  {
+        "N":2,
+        "archivio":"Archivio Bonsanti",
+        "fondo":"Fondo Gadda",
+        "unit\u00e0":"\u00abPiccola antologia 1904\u00bb",
+        "serie":"II",
+        "sottoserie":"1 Quaderni 1",
+        "faldone":null,
+        "opera":"Appunti giovanili, scolastici e universitari (politecnico)",
+        "Schede":"Appgens",
+        "lib":null,
+        "luogo":null,
+        "genere":"Appunti",
+        "forma":"Quaderni",
+        "tipo":"Documento",
+        "contenitore":"1 scatola",
+        "sigla":null,
+        "Unnamed: 16":null,
+        "descrizione":null,
+        "provenienza":null,
+        "lingua":null,
+        "sommario":null,
+        "stato di conservazione":null,
+        "collana":null,
+        "data":-2082844800000
+    }];
+    const file2Data =[{
+        "Id":3.0,
+        "Nota di possesso":"\u00abCarlo Emilio Gadda \u2013 ottobre 1931-\u00bb",
+        "Dedica":null,
+        "Collocazione":"[p. 61 rr. 1-9 \u00abMais nous commen\u00e7ons \u00e0 conna\u00eetre la d\u00e9marche de ses anxi\u00e9t\u00e9s nocturnes: a nous commen\u00e7ons \u00e0 savoir le point \u00e0 partir duquel Balzac se s\u00e9pare de son exp\u00e9rience v\u00e9cue pour forger l'\u00eatre fictif auquel il va communiquer toute la force tragique de ses propres imaginations. Si la mort de l'enfant, si la mort de la jeune fille s\u00e9duite, si ce fait-divers bouleversant n'a jamais exist\u00e9 pour lui, est-on en droit de dire que rien n'aurait pu le causer?\u00bb].",
+        "Postilla parlata":"dopo la promesse { \u201cmeschinit\u00e0 --> dalle immagini alla biografia",
+        "Postilla muta":"Sottolineature"
+    },{
+        "Id":3.0,
+        "Nota di possesso":null,
+        "Dedica":null,
+        "Collocazione":"[p. 62 rr. 7-20 \u00abEn fait, la ressemblance entre le Benassis de cinquante ans et l'\u00e9crivain de trente-trois acquiert toute la valeur d'une conjuration lointaine. Comme Balzac a conjur\u00e9 ses spectres de nagu\u00e8re, il s'efforce ici de conjurer le spectre de son \u00e2ge m\u00fbr. La trag\u00e9die est la m\u00eame, qu'il s'agisse de briser avec le pass\u00e9 ou de se lib\u00e9rer du futur. Fant\u00f4mes d'hier ou fant\u00f4mes de demain, les \u00eatres model\u00e9s \u00e0 la ressemblance de leur cr\u00e9ateur assument, au long de la \u201cCom\u00e9die Humaine\u201d, cette t\u00e2che affreuse de lui conqu\u00e9rir et de lui maintenir la route libre. A cette t\u00e2che ils succombent l'un apr\u00e8s l'autre. Mais leur sacrifice n'est pas vain: la route demeure ouverte, et l'\u00e9crivain peut s'y donner carri\u00e8re\u00bb]",
+        "Postilla parlata":"Conclusione sintetica",
+        "Postilla muta":null
+    }]
+
+
+// Assuming you have an element with id="matchingItems" in your HTML
+const matchingItemsContainer = document.getElementById('matchingItems');
+
+// Iterate through each item in jsonData
+jsonData.forEach((item) => {
+  // Check if the "Schede" key is present in the current item
+  if (item.Schede) {
+    // Filter jsonDataArch to find items with the same "Schede" value
+    const matchingArchItems = jsonDataArch.filter((archItem) => archItem.Schede === item.Schede);
+
+    // Display the matching items from jsonDataArch
+    matchingArchItems.forEach((archItem) => {
+      // Create a new div element for each matching item
+      const matchingItemDiv = document.createElement('div');
+      
+      // Set the inner HTML of the div to the matching item details
+      matchingItemDiv.innerHTML = `Matching item in jsonDataArch: ${JSON.stringify(archItem)}`;
+
+      // Append the div to the container in your HTML
+      matchingItemsContainer.appendChild(matchingItemDiv);
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+     // Rest of your List.js code
+   const options = {
+     valueNames: ['Authors', 'Title', 'Cities', 'Publisher', 'Years', 'Fund', 'Category', 'Schede'],
+     page: 5,
+     pagination: true,
+     item: function (item, options) {
+       // Create an array of links for the matching items in file2Data
+       const matchingItems = file2Data.filter((file2Item) => file2Item.Id === item.Id);
+       let links = '';
+   
+       if (matchingItems.length > 0) {
+        // Build the URL for the Postille.html page with the item Id as a parameter
+        const url = `Postille.html?id=${item.Id}`;
+    
+        // Create the link with an onclick event to open the URL in a new window
+// ...
+
+links = `<a href="Postille.html?id=${item.Id}&title=${encodeURIComponent(item.Title)}" onclick="window.open('${url}', '_blank'); return false;">ANNOTATIONS</a>`;
+
+    }
+    
+   
+         
+   
+       return `<li>
+       <strong class="Authors">${item.Authors}</strong><br>
+       <span class="Title">${item.Title}</span><br>
+       <span class="Cities">${item.Cities}</span>:
+       <span class="Publisher">,</span>,
+       <span class="Years">${item.Years}</span><br>
+       ${item.Fund ? `<span style="font-family: 'Nunito', sans-serif; font-size: 14px;color: #b7491d;font-weight: 800;">FUND:</span> ${item.Fund}<br>` : ''}   
+       ${item.Schede ? `<span style="cursor: pointer; text-decoration: underline; color: blue;" class="schede-link" data-schede="${item.Schede}">SCHEDE: ${item.Schede}</span><br>` : ''}
+ 
+      ${item.Category ? `<span style="font-family: 'Nunito', sans-serif; font-size: 13px;color: #b7491d;font-weight: 800;">GENRE: </span> ${item.Category}` : ''}<br>
+           ${item.Segni ? `<span style="font-family: 'Nunito', sans-serif; font-size: 14px;color: #b7491d;font-weight: 800;"> ${links}</span>: ${item.Segni}<br>` : ''}
+       ${!item.Segni ? `${links}` : ''}
+     </li>`
+
+     
+
+     }
+   }
+   const userList = new List('catalog', options, jsonData);
+});
+
+// ... Your existing code ...
+
+// Assuming you have an element with id="matchingItems" in your HTML
+
+// Add an event listener to the document for clicks on schede links
+document.addEventListener('click', function (event) {
+    const target = event.target;
+  
+    // Check if the clicked element has the class "schede-link"
+    if (target.classList.contains('schede-link')) {
+      // Get the value of the "data-schede" attribute
+      const schedeValue = target.getAttribute('data-schede');
+  
+      // Split the comma-separated values into an array
+      const schedeArray = schedeValue.split(',').map(value => value.trim());
+  
+      // Filter jsonDataArch to find items with any of the "Schede" values
+      const matchingArchItems = jsonDataArch.filter((archItem) => schedeArray.includes(archItem.Schede));
+console.log('Matching Items:', matchingArchItems);
+
+      // Display the matching items from jsonDataArch on archive.html
+      showMatchingItems(matchingArchItems);
+      
+      // Open the archive.html file in a // Open the archive.html file in a new window with schedeValue as a query parameter
+window.open(`Archive2.html?schedeValue=${encodeURIComponent(schedeValue)}`, '_blank');
+
+    }
+  });
+  
+  function showMatchingItems(matchingItems) {
+    // Get the existing "matchingItems" div from the HTML
+    const matchingItemsContainer = document.getElementById('matchingItems2');
+  
+    // Clear the existing content
+    matchingItemsContainer.innerHTML = '';
+  
+    // Append each matching item to the container
+    matchingItems.forEach((archItem) => {
+      const matchingItemDiv = document.createElement('div');
+      matchingItemDiv.innerHTML = `Matching item in jsonDataArch: ${JSON.stringify(archItem)}`;
+      matchingItemsContainer.appendChild(matchingItemDiv);
+    });
+  
+    // You can also add styling or modify the display logic as needed
+  }
+  
+
+
+
